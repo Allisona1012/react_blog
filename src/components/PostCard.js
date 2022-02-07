cff5457bf1c09c7f492b068f52d1723575210e97
Allisona1012 <allisona1012@gmail.com>
2022-02-07 10:36:07 -0600

    a new homework commit

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -6,8 +6,8 @@ export default class PostCard extends Component {
         <div className="card p-3 mt-3 bg-light text-dark border border-secondary border-3 ">
         <div className="card-body">
             <h5 className="card-title">{ this.props.post.title }</h5>
-            <h6 className="card-title">{ this.props.post.username }</h6>
-            <p className="card-text">{ this.props.post.body }</p>
+            <h6 className="card-title">{ this.props.post.author.username }</h6>
+            <p className="card-text">{ this.props.post.content }</p>
         </div>
     </div>
 
